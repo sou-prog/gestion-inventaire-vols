936d7dd467dda9e8852972b1c8f00e4b94efd33c
IKGUESS <ikramguessous2003@gmail.com>
2024-12-24 00:22:23 +0100

    Update flight_service.js

--- frontend/src/services/flight_service.js
+++ frontend/src/services/flight_service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/flight";
+const API_URL = "http://localhost:8080/api/vols";
 
 // Adds a flight by making a POST request to the API
 const addFlight = (
