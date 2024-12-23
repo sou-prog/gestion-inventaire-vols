ff214f7fd4fae6805b3615a00417f827e3f9264e
IKGUESS <ikramguessous2003@gmail.com>
2024-12-24 00:18:49 +0100

    Update booking_service.js

--- frontend/src/services/booking_service.js
+++ frontend/src/services/booking_service.js
@@ -1,6 +1,6 @@
 // booking_service.jsx
 
-const BASE_URL = "http://localhost:8080/api/v1/booking/saveBooking"; // Replace this with the appropriate base URL for your backend API
+const BASE_URL = "http://localhost:8080//api/reservations"; // Replace this with the appropriate base URL for your backend API
 
 const addBooking = async (bookingData) => {
   try {
