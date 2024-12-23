1cd27fd67eee0fa9b9006eb0f7402ad47940f0f8
IKGUESS <ikramguessous2003@gmail.com>
2024-12-24 00:16:58 +0100

    Update auth.service.js

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/auth";
+const API_URL = "http://localhost:8080/auth/api";
 
 // Registers a user
 const signup = (firstname, lastname, username, password, role) => {
